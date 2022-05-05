30613667469bc60fc5bb5034bcf8ed91bea3ea0f
maureenndiema <ndiemam@gmail.com>
2022-05-05 17:19:41 +0300

    create tests

---
+++ tests/test_review.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Review
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_movie = Review(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Review))
